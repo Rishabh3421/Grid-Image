918da19fbc7a70620721ef9fda0c4242fd348c5f
Rishabh3421 <134938190+Rishabh3421@users.noreply.github.com>
2023-06-16 23:00:13 +0530

    Add files via upload

---
+++ Grid/script.js
@@ -0,0 +1,32 @@
+const images = [
+    'https://images.unsplash.com/photo-1686903430777-279ba0f25e7f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=400&q=60',
+    'https://images.unsplash.com/photo-1682685797365-41f45b562c0a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=400&q=60',
+    'https://images.unsplash.com/photo-1686719277776-93484f0de0f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
+    'https://images.unsplash.com/photo-1686685699350-5491830a18c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
+    'https://images.unsplash.com/photo-1686740172488-6a680069affb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
+    'https://images.unsplash.com/photo-1661956601031-4cf09efadfce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
+    'https://images.unsplash.com/photo-1686772939025-20195731d9f1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
+    'https://plus.unsplash.com/premium_photo-1686604504223-47a6aef5c3f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
+    'https://images.unsplash.com/photo-1661956603025-8310b2e3036d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw0OHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
+    'https://images.unsplash.com/photo-1686644823329-c176dffccbe3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2MXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
+    'https://images.unsplash.com/photo-1686577184198-9663fd674cdc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2Nnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
+    'https://plus.unsplash.com/premium_photo-1673676884428-ad8c7271c16e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60'
+];
+
+const gridContainer = document.getElementById('imageGrid');
+
+images.forEach((image) => {
+    const gridItem = document.createElement('div');
+    gridItem.className = 'grid-item';
+
+    const img = document.createElement('img');
+    img.src = image;
+
+    const tooltip = document.createElement('span');
+    tooltip.className = 'tooltip';
+    tooltip.innerText = 'Image Grid';
+
+    gridItem.appendChild(img);
+    gridItem.appendChild(tooltip);
+    gridContainer.appendChild(gridItem);
+});
